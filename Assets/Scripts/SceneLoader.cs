b5fb304be46ff6ac2c813096cbbc8d409372e703
Bouchaud <o.bouchaud@etudiant.ecolemelies.fr>
2020-06-18 14:02:53 +0200

    SceneLoader script updated;

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private string sceneToLoadName;
+
+public void LoadScene()
+{
+    StartCoroutine(Load());
+}
+
+private IEnumerator Load()
+{
+    var loadingScreenInstance = Instantiate(loadingScreen);
+    //contains the Prefab
+    var loadingAnimator = loadingScreenInstance.GetComponent<Animator>();
+
+    var animationTime = loadingAnimator.GetCurrentAnimatorStateInfo(0).length;
+    //getting the duration of our animations
+
+    DontDestroyOnLoad(loadingScreenInstance);
+    var loading = SceneManager.LoadSceneAsync(sceneToLoadName);
+    //will open the scene once it is fully charged
+
+    loading.allowSceneActivation = false;
+    //we don't automatically load the scene when it's fully charged
+
+    while (loading.progress < 0.9f)
     {
-        
+        yield return new WaitForSeconds(animationTime);
+        //yield = waiting
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    loading.allowSceneActivation = true;
+    loadingAnimator.SetTrigger("EndLoading");
+}
 }
