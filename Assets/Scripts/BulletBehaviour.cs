e952739a97d99c57bfa39636004d71871b3227a9
Bouchaud <o.bouchaud@etudiant.ecolemelies.fr>
2020-06-18 13:11:17 +0200

    BulletBehaviour Script updated;

--- Assets/Scripts/BulletBehaviour.cs
+++ Assets/Scripts/BulletBehaviour.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class BulletBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float speed;
+    private Transform player;
+    private Vector2 target;
+
+
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        target = new Vector2 (player.position.x, player.position.y);
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if(transform.position.x == target.x && transform.position.y == target.y)
+        {
+            DestroyBullet();
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            DestroyBullet();
+        }
+    }
+    void DestroyBullet()
+    {
+        Destroy(gameObject);
     }
 }
