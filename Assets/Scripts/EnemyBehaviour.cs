24ecb209d4ec3f3a85d58408ac16c4bdf3c0aa71
Bouchaud <o.bouchaud@etudiant.ecolemelies.fr>
2020-06-18 13:34:47 +0200

    EnemyBehaviour Script Updated;
    Now move towards Player;

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
+    public float speed;
+    public Transform player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+       
     }
 }
