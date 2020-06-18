5f416a13abe85473208aaa07b3eaf104c86db0cf
Bouchaud <o.bouchaud@etudiant.ecolemelies.fr>
2020-06-18 12:59:02 +0200

    PlayerBehaviour's script updated;
    Trying to make the Player move;

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -6,6 +6,11 @@ using UnityEngine.InputSystem;
 public class PlayerBehaviour : MonoBehaviour
 {
 
+    [SerializeField] private float speed;
+
+    private Vector2 stickDirection;
+    private Rigidbody2D myRB2D;
+
     private void OnEnable()
     {
         var playerControls = new PlayerControls();
@@ -18,29 +23,31 @@ public class PlayerBehaviour : MonoBehaviour
 
     private void ShootOnperformed(InputAction.CallbackContext obj)
     {
-
+        //Instantiate a bullet
     }
 
     private void MoveOnperformed(InputAction.CallbackContext obj)
     {
-
+        stickDirection = obj.ReadValue<Vector2>();
     }
 
     private void MoveOncanceled(InputAction.CallbackContext obj)
     {
-
+        stickDirection = Vector2.zero;
     }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRB2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        var direction = new Vector2{ x = stickDirection.x, y = stickDirection.y};
+        myRB2D.AddForce(direction * speed);
+
     }
 }
