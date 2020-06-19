0dfe1038fa603b4da59269749fabecb92f595393
Bouchaud <o.bouchaud@etudiant.ecolemelies.fr>
2020-06-19 20:43:27 +0200

    PlayerBehaviour Script updated;
    Succeeded to rotate the player around Mouse pos, but not with a normalized Vector yet;

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -42,6 +42,23 @@ public class PlayerBehaviour : MonoBehaviour
 
     private void RotateOnperformed(InputAction.CallbackContext obj)
     {
+    //Pour récupérer un vecteur équivalent à la direction entre 2 points, la formule est : Point d'arrivée - point de départ. 
+    //À ce titre la direction d'un objet A à la position (5,5) vers un objet B à la position (7,9) sera la suivante : (7-5,9-5) = (2,4)
+    //Il faut noter aussi la possibilité de réduire la taille de ce vecteur à 1 en le normalisant à la suite des calculs.
+
+    
+    //Vector2.normalized
+    Vector2 positionOnScreen = Camera.main.WorldToViewportPoint (transform.position);
+    //Vector2 mouseDirection = new Vector2(obj.ReadValue<Vector2>().x, obj.ReadValue<Vector2>().y);
+    Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(obj.ReadValue<Vector2>());
+    float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+    transform.rotation =  Quaternion.Euler (new Vector3(0f,0f,angle));
+    
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b) 
+     {
+         return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+     }
+
 
     }
 
